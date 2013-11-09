e179a349f0335d3ffaaf55711a0583a40d7fa0ee
Neville Davey <nevilledavey@gmail.com>
2013-11-08 17:51:39 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Insuresave::Application.config.secret_key_base = '70a8ab814e17ea9588c6a7f7bfce05731d862e713e2cb50336a40854fe8bca37344051fde5321f6ce77485edb7d820f31ef7b338084d1a78b4082201f1613f7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Insuresave::Application.config.session_store :cookie_store, key: '_insuresave_session'
